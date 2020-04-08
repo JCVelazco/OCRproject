5e03c7830513383a987c17c83b7a773879d2d043
JCVelazco <juan.carlosvr.97@hotmail.com>
2020-04-08 02:48:39 -0500

    add wolf thresholding, add different methods to clean the image and put boxes int the data

--- pPruebas.py
+++ pPruebas.py
@@ -3,6 +3,7 @@ import numpy as np
 import udF2
 import time
 import sys
+import matplotlib.pyplot as plt
 sys.setrecursionlimit(350000)
 
 start_time = time.time()
@@ -14,7 +15,7 @@ img = udF2.imgRS(img,0.5) #Este resize está solo para hacer más rápidas las p
 
     #-Binarización de la imagen
 
-    #Lo seguiente se activa en caso de querer usar un solo niblack
+        #Un solo niblack
 winSize = 65
 img1 = udF2.niblack(img, winSize, -2)
 print("Niblack done")
@@ -23,39 +24,46 @@ img1 = udF2.invert(img1)
 print("Invert done")
 print("--- %s seconds ---" % (time.time() - start_time))
 
-    #Uso de 5 niblacks
-# img1 = udF2.cincoNiblack(img)
-# print("Niblack done")
-# print("--- %s seconds ---" % (time.time() - start_time))
-
-    #-Quitar el padding que se puso en niblack, a fin de obtener una imagen del tamaño original
-    #Solo aplcia si no se usan los 5 niblacks
-
 img1 = udF2.getImgBk(img1, winSize)
 print("getBack done")
 print("--- %s seconds ---" % (time.time() - start_time))
 
-    #checa vecindad con los 8 vecinos, pero solo son requeridos los de arriba y el centro izquierda. NO ENCUENTRA TRASLAPES DE OBJETOS!!!!!!!!!!!!!!!!!!!
+        # Uso de 5 niblacks
+# img1 = udF2.cincoNiblack(img)
+# print("Niblack done")
+# print("--- %s seconds ---" % (time.time() - start_time))
+
+
+        #Operaciones morfológicas
+# kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
+# img1 = cv2.morphologyEx(img1, cv2.MORPH_OPEN, kernel)
+# print("Morph done")
+# print("--- %s seconds ---" % (time.time() - start_time))
+
+    #Busqueda de objetos y propiedades
+        #Busqueda de regiones en la imagen
 
 objMtx, nObj = udF2.objSrch2(img1)
 print("N de objetos encontrados =",nObj)
 print("objTag done")
 print("--- %s seconds ---" % (time.time() - start_time))
 
-    #Boxing de OBJETOS
+        #Boxing de OBJETOS
 
 boxesLst = udF2.boxing(objMtx, nObj)
+boxesLst = udF2.boxCleaning(boxesLst,img1)
 print("Boxing done")
 print("--- %s seconds ---" % (time.time() - start_time))
 
-    #colorea de un color aleatorio cada objeto distinto
-
-imgColored = udF2.rgbObjColor(objMtx,nObj)
+        #Medidas estadísticas
+        
+    #Efectos visuales
+h,w = boxesLst.shape
+imgColored = udF2.rgbObjColor(objMtx,h)
 print("Coloring done")
 print("--- %s seconds ---" % (time.time() - start_time))
 
-    #Esto aun no jala
-imgColored = udF2.DrawSq(imgColored,boxesLst)
+imgSq = udF2.DrawSq(imgColored,boxesLst)
 print("Square drawing done")
 print("--- %s seconds ---" % (time.time() - start_time))
 
